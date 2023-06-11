fe8f3f36fd3101e7651853c1df05e3d4ed958c69
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-06-11 19:28:13 +0200

    add button

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from "./components/button";
+import "./styles/style.scss";
+
+
+function App() {
+  return (
+    <>
+   <Button text="Click me"/>
+   <Button text="Opet click" color="red"/>
+    </>
+  )
+}
+
+export default App;
